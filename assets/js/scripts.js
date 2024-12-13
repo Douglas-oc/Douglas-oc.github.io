59e46f8b473892ec2777985058b20fae62eeec0f
Douglas <142617844+Douglas-oc@users.noreply.github.com>
2024-12-13 16:52:44 +0000

    finalizado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -27,5 +27,12 @@ accordionHeaders.forEach(header => {
     })
 })
 
+const menuLinks = document.querySelectorAll(".menu__link");
 
+menuLinks.forEach(item => {
+    item.addEventListener("click", ()=> {
+      menuLinks.forEach(i => i.classList.remove("active"));
+      item.classList.add("active");  
+    })
+})
 
